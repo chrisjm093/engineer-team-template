bdc26f6bab7a404a6690dc78e49ca5000c7153b6
Christopher Marti <chrisjm093@gmail.com>
2020-06-08 21:08:04 -0700

    Manager class and constructor built, test passes

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,7 +1,7 @@
 const Employee = require("../lib/Employee.js");
 
 class Engineer extends Employee{
-       ;
+       
     constructor(name, id, email, github){
         super(name, id, email);
         this.github = github;
